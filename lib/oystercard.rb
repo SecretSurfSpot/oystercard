c68f10ab7f5f980d513df4b8d810f187b3551f7e
Andrew Gibbs <asg1968@hotmail.co.uk>
2018-08-14 12:24:25 +0100

    Add fail code to #top_up if CARD_LIMIT exceeded

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,4 +1,5 @@
 class Oystercard
+  CARD_LIMIT = 90
   attr_accessor :balance
 
   def initialize
@@ -6,6 +7,10 @@ class Oystercard
   end
 
   def top_up(amount)
+    # line below is in walkthrough and replaces the subsequent 2 lines
+    fail "Maximum balance of #{CARD_LIMIT} exceeded" if amount + balance > CARD_LIMIT
+    # value = balance + amount
+    # raise "Invalid request - Balance will exceed £#{CARD_LIMIT}" if value > 90
     self.balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -21,5 +21,25 @@ describe Oystercard do
     expect(card.balance).to eq(amount)
   end
 
+  it "raises an error if the maximum balance is exceeded" do
+    maximum_balance = Oystercard::CARD_LIMIT
+    subject.top_up(maximum_balance)
+    expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+  end
+  # Code above on walkthrough and replaces code below:
+  # it "raises an error - attempt to raise card balance over £90" do
+  #   card = Oystercard.new
+  #   #card.top_up(100)
+  #   expect { card.top_up(91) }.to raise_error "Invalid request - Balance will exceed £#{Oystercard::CARD_LIMIT}"
+  # end
+
+
+
+# experimental rspec code
+  # it "expect card to be an instance of Oystercard" do
+  #   card = Oystercard.new
+  #   expect(card).to be_an_instance_of(Oystercard)
+  # end
+
 
 end
